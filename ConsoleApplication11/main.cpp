fc7bf8ed0f29e9447fd3bc453597cc72b2d2c811
unknown <p42z3@gmail.com>
2014-06-09 17:22:05 +0300

    main changed

--- ConsoleApplication11/main.cpp
+++ ConsoleApplication11/main.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	int a = 155, b = 10;
 	int res;
-	res = add (a,b);
-	display(res);
-	res = mult (a,b);
-	display(res);
+	//res = add (a,b);
+	//display(res);
+	//res = mult (a,b);
+	//display(res);
 }
